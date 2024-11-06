83961d5796f7aa7cadde6310966515d282587031
Abdul Waheed <abdulwaheed19026@gmail.com>
2024-11-06 16:41:31 +0500

    Data Validation Completed

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from TextSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from TextSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from TextSummarizer.logging import logger
 
 
-STAGE_NAME = "data_ingestion_stage"
+STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>> Stage {STAGE_NAME} started <<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>> Stage {STAGE_NAME} completed <<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>> Stage {STAGE_NAME} started <<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>> Stage {STAGE_NAME} completed <<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
